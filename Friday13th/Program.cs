293c7df551821bb1184a8f7807c9b232ebf3c7c5
Danman05 <110387670+Danman05@users.noreply.github.com>
2022-08-16 17:47:36 +0200

    Add files via upload

---
+++ Friday13th/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Friday13th
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // for loop runs 12 times, because there is 12 months in a year
+            for (int i = 1; i <= 12; i++)
+            {      
+                // writes to console if, the friday is the 13th 
+                DateTime date = new DateTime(DateTime.Today.Year + 1, i, 13);
+
+                Console.WriteLine( date);
+                // if condition is met, it writes to console if, the friday is the 13th 
+                if (date.DayOfWeek == DayOfWeek.Friday)
+                {
+                    Console.WriteLine(date.ToString("dd\\t\\h MMMM yyyy") + " er en fredag");
+                }
+            }
+           
+        }
+    }
+}
