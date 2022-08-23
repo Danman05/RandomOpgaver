96811e6eac2762de2e1723aba129ce5ef9753d95
Danman05 <110387670+Danman05@users.noreply.github.com>
2022-08-23 17:21:41 +0200

    Update Program.cs

--- Storebæltsforbindelsen/Program.cs
+++ Storebæltsforbindelsen/Program.cs
@@ -18,6 +18,7 @@ namespace Storebæltsforbindelsen
             Console.ReadLine();
             
         }
+        // Calculates motorcycleTires from input with FirstUserInput
         static int CalculationMotorcycle()
         {
             int motorcycle = FirstUserInput();
@@ -25,30 +26,35 @@ namespace Storebæltsforbindelsen
 
             return motorcycleTire;
         }
+        // Calculates carsTire from input with SecondUserInput
         static int CalculationCar()
         {
             int cars = SecondUserInput();
             int carsTire = cars * 4;
             return carsTire;
         }
+        // Calculates busTire from input with ThirdUserInput
         static int CalculationBus()
         {
             int bus = ThirdUserInput();
             int busTire = bus * 6;
             return busTire;
         }
+        // First user input, converts to an int and returns the value
         static int FirstUserInput()
         {
             string input = Console.ReadLine();
             int answer = Int32.Parse(input);
             return answer;
         }
+        // Second user input, converts to an int and returns the value
         static int SecondUserInput()
         {
             string input = Console.ReadLine();
             int answer = Int32.Parse(input);
             return answer;
         }
+        // Third user input, converts to an int and returns the value
         static int ThirdUserInput()
         {
             string input = Console.ReadLine();
